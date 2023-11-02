76aaaa67c59f5ff4c45f337c7714d2be801133a2
Stefan <trsunovstefan@gmail.com>
2023-11-02 10:17:44 +0100

    Initial commit

--- vehicle-detection-classifier/screenshot.py
+++ vehicle-detection-classifier/screenshot.py
@@ -4,12 +4,12 @@ from carDetectClassifier import *
 from dict import *
 
 # trafic_image = Image.open('/home/stefan/personal/traffic-emissions-prediction/vehicle-detection-classifier/traffic2.jpeg') # from which image to open and take the contents
-trafic_image = Image.open('/home/stefan/personal/traffic-emissions-prediction/vehicle-detection-classifier/traffic.png')
+trafic_image = Image.open('./traffic.png')
 my_dict_value = list(my_dict.values())
 my_dict_key = list(my_dict.keys())
 for i in range(len(my_dict_key)):
     crop_car = trafic_image.crop(my_dict_value[i]) # coordinates where to crop
-    crop_car.save(f'/home/stefan/personal/traffic-emissions-prediction/vehicle-detection-classifier/images/{my_dict_key[i]}.jpeg')# where to save the images
+    crop_car.save(f'./images/{my_dict_key[i]}.jpeg')# where to save the images
 
 
 
